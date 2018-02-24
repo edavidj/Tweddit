9fe9f7415030d2ef7a86ef7780b40d3fa429f83f
Ethan Johnston <johnse20@mcmaster.ca>
2018-02-24 03:55:45 -0500

    updated for reddit

--- src/Reddit.js
+++ src/Reddit.js
@@ -1,6 +1,17 @@
 module.exports = (function(){
     //private functions
+    /**
+     * Get related subreddits for each tag and create multi reddit from it
+     * @param {Express Object} res res.send to send multi reddit link to front end
+     * @param {String[]} tags tags from twitter analysis
+     */
+    function createMulti(res,tags){
+        //do the thing
+    }
     return {
         //public methods
+        createMulti: function(res, tags){
+            createMulti(res,tags);
+        }
     };
 })();
--- src/Twitter.js
+++ src/Twitter.js
@@ -7,13 +7,24 @@ module.exports = (function(){
      * Get timeline posts
      * @param {String} user 
      */
+    var parseIndico = (res) => {
+        //clarify that res structure is just api response
+        let maxKey = "anime";
+        let keys = Object.keys(res);
+        for(var i of keys){
+            if(res[i] > res[maxKey]) { 
+                maxKey = res[i];
+            }
+        }
+        return maxKey;
+    }
     function getTags(req,res){
         let user = req.query.q;
         let url ="";
         Request(url, (posts) => {
             let tags = [];
             for(var i = 0; i < posts.length; i++){
-
+                
             }
 
         });
